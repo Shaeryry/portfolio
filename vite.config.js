9eecc83562e2a4c7b8e33e28a31704be323378ad
Shaeryry <Shaeryry@users.noreply.github.com>
2025-09-06 19:19:54 +0000

    deploy: Shaeryry/portfolio-source@398eb95934c3765f434a53742c3498369fb93019

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// Replace "portfolio" with your repo name
-export default defineConfig({
-  plugins: [react()],
-  base: "/portfolio/",
-});
